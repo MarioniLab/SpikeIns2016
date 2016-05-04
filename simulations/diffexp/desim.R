9437cfc6941ef875b15044ea3f73356a97f13b34
Aaron Lun <alun@wehi.edu.au>
2016-05-04 19:10:04 +0100

    Added MAST, removed QL, cleaned up monocle for DE simulations.

--- simulations/diffexp/desim.R
+++ simulations/diffexp/desim.R
@@ -3,14 +3,15 @@
 # exploiting the presence of spike-ins for normalization.
 
 suppressPackageStartupMessages(require(simpaler))
+suppressPackageStartupMessages(require(scran))
 suppressPackageStartupMessages(require(edgeR))
-suppressPackageStartupMessages(require(SAMstrt))
 suppressPackageStartupMessages(require(monocle))
+suppressPackageStartupMessages(library(MAST))
 
 # Note that Kharchenko's SCDE package doesn't seem to support spike-in normalization.
 
 fdr.threshold <- 0.05
-top.hits <- c(20, 200, 2000)
+top.hits <- 200
 
 temp <- "temp.txt"
 if (file.exists(temp))  { unlink(temp) }
@@ -18,7 +19,7 @@ filled <- FALSE
 
 #################################################################################
 
-for (datatype in c("brennecke", "islam")) {
+for (datatype in c("kolod", "islam")) {
 	if (datatype=="islam") {
 		counts <- read.table("GSE29087_L139_expression_tab.txt.gz", 
 			colClasses=c(list("character", NULL, NULL, NULL, NULL, NULL, NULL), rep("integer", 96)), skip=6, sep='\t', row.names=1)
@@ -26,30 +27,35 @@ for (datatype in c("brennecke", "islam")) {
 		grouping <- factor(c(rep(c("ESC", "MEF", "Neg"), c(48, 44, 4))))
 		
 		# Quality control on individual cells.
-		totals <- colSums(counts[!is.spike,])
+		totals <- colSums(counts)
 		is.mito <- grepl("^mt-", rownames(counts)) & !is.spike
-		okay.libs <- totals >= 1e5 & colSums(counts[is.mito,])/totals < 0.1 
+		okay.libs <- !isOutlier(totals, n=3, type="lower", log=TRUE) &
+                     !isOutlier(colSums(counts[is.mito,])/totals, n=3, type="higher") & 
+                     !isOutlier(colSums(counts[is.spike,])/totals, n=3, type="higher")
 		counts <- counts[,okay.libs]
 		grouping <- droplevels(grouping[okay.libs])
 		
-	} else if (datatype=="brennecke") {
-		countsAll <- read.table('nmeth.2645-S10.csv.gz', header=TRUE, row.names=1, sep=',', colClasses=c('character', rep('integer', 13)))
-		grouping <- factor(substr(colnames(countsAll), 0, 2))
-		
-		geneTypes <- factor( c( AT="At", pG="pGIBS", EN="HeLa", ER="ERCC" )[ substr( rownames(countsAll), 1, 2 ) ] )
-		is.HeLa <- which( geneTypes=="HeLa" )
-		countsHeLa <- countsAll[ is.HeLa, ]
-		countsAt <- countsAll[ which( geneTypes=="At" ), ]
-		
-		rownames(countsHeLa) <- paste0("RNA_SPIKE_", rownames(countsHeLa)) # for SAMstrt, which fails unless we give it names.
-		counts <- as.matrix(rbind(countsAt, countsHeLa))
-		is.spike <- c(logical(nrow(countsAt)), !logical(nrow(countsHeLa)))
+	} else if (datatype=="kolod") {
+        all.counts <- read.table("ESpresso/counttable_es.csv", header=TRUE, row.names=1, colClasses=c("character", rep("integer", 704)))
+        serum <- sub("ola_mES_([^_]+)_.*", "\\1", colnames(all.counts))
+        batch <- sub("ola_mES_[^_]+_([^_]+)_.*", "\\1", colnames(all.counts))
+        targets <- data.frame(Serum=serum, Batch=batch)
+                                            
+        # Only using data from the batch with spike-ins.
+        keep <- targets$Batch=="3" & targets$Serum %in% c("lif", "2i")
+        counts <- all.counts[,keep]
+        is.spike <- grepl("^ERCC", rownames(counts))
+        is.mouse <- grepl("^ENSMUSG", rownames(counts))
+
+        # Getting rid of mapping statistics stored in the same file.
+        counts <- counts[is.spike|is.mouse,] 
+        is.spike <- is.spike[is.spike|is.mouse]
+        grouping <- factor(targets$Serum[keep])
 	}
 
 	design <- model.matrix(~grouping)
-	colnames(design) <- levels(grouping)
-    spike.param <- spikeParam(counts[is.spike,])
-	filter.keep <- rowSums(counts) >= ncol(counts)
+	filter.keep <- rowMeans(counts) >= 1
+    spike.param <- spikeParam(counts[is.spike & filter.keep,])
 
 	#################################################################################
 	# Running edgeR first.
@@ -60,7 +66,7 @@ for (datatype in c("brennecke", "islam")) {
 
     for (my.var in c(0.001, 0.01, 0.1)) {
 		set.seed(231234)
-		results <- top.res <- list()
+		results <- top.set <- list()
 		
 		for (i in seq(0,20)) {
 			# Running original data, then resampled versions.
@@ -71,29 +77,25 @@ for (datatype in c("brennecke", "islam")) {
 			y <- y.ref
 			y$samples$norm.factors <- colSums(spike.data)/y$samples$lib.size
 
-			# Running QL edgeR.
+			# Running edgeR.
 			y <- estimateDisp(y, design)
-			fit <- glmQLFit(y, design, robust=TRUE)
-			res <- glmQLFTest(fit)
+			fit <- glmFit(y, design, robust=TRUE)
+			res <- glmLRT(fit)
 
 			# Check if diagnostics look okay:
 			if (!diag.done) {
 				pdf(paste0("diagnostics_", datatype, ".pdf"))
 				plotBCV(y) # Nice downward trend
 				plotMDS(cpm(y, log=TRUE), col=c("red", "blue")[(as.integer(grouping)==1)+1L]) # Mostly separated
-				plotQLDisp(fit) # Funny parabolic shape, but typical of single-cell data where there's loads of zeros.
 				dev.off()
 			}
 
 			# Comparing.
 			chosen <- p.adjust(res$table$PValue, method="BH") <= fdr.threshold
-		    my.rank <- rank(res$table$PValue) 
-            top.ranked <- lapply(top.hits, function(x) { my.rank <= x })
+		    top.ranked <- rank(res$table$PValue) <= top.hits
 			if (i) { 
-				lost <- sum(original & !chosen)/sum(original)
-				gained <- sum(!original & chosen)/sum(chosen)
-				results[[i]] <- c(lost, gained)
-				top.res[[i]] <- sapply(seq_along(top.hits), function(j) { sum(best[[j]] & !top.ranked[[j]])/top.hits[j] })
+				results[[i]] <- sum(original!=chosen)/sum(original)
+				top.set[[i]] <- sum(best!=top.ranked)/top.hits
 			} else {
 				original <- chosen
 				best <- top.ranked
@@ -109,8 +111,8 @@ for (datatype in c("brennecke", "islam")) {
 		}
 
 		final <- do.call(rbind, results)
-        colnames(final) <- c("Lost", "Gained")
-		top.lost <- do.call(rbind, top.res)
+        colnames(final) <- "Total"
+		top.lost <- do.call(rbind, top.set)
         colnames(top.lost) <- paste0("Top", top.hits)
 
         write.table(file=temp, data.frame(Dataset=datatype, Variance=my.var, Method="edgeR", Total=length(original), Detected=sum(original),
@@ -119,62 +121,7 @@ for (datatype in c("brennecke", "islam")) {
 	}
 
 	#################################################################################
-	# Also running NB models without any EB shrinkage, only using the tagwise dispersions.
-
-	diag.done <- FALSE
-    for (my.var in c(0.001, 0.01, 0.1)) {
-		set.seed(231234)
-		results <- top.res <- list()
-		
-		for (i in seq(0,20)) {
-			# Running original data, then resampled versions.
-			if (i) { spike.data <- resampleSpikes(spike.param, var.log=my.var) }
-			else { spike.data <- spike.param$counts }
-		
-			# Normalizing on the spike-in counts.
-			y <- y.ref
-			y$samples$norm.factors <- colSums(spike.data)/y$samples$lib.size
-
-			# Running no-shrinkage edgeR.
-			y <- estimateDisp(y, design, prior.df=0, trend.method="none")
-			fit <- glmFit(y, design)
-			res <- glmLRT(fit)
-
-			# Comparing.
-			chosen <- p.adjust(res$table$PValue, method="BH") <= fdr.threshold
-            my.rank <- rank(res$table$PValue) 
-            top.ranked <- lapply(top.hits, function(x) { my.rank <= x })
-			if (i) { 
-				lost <- sum(original & !chosen)/sum(original)
-				gained <- sum(!original & chosen)/sum(chosen)
-				results[[i]] <- c(lost, gained)
-				top.res[[i]] <- sapply(seq_along(top.hits), function(j) { sum(best[[j]] & !top.ranked[[j]])/top.hits[j] })
-			} else {
-				original <- chosen
-				best <- top.ranked
-			}
-
-			if (!diag.done) {
-				# Top genes also look like the ones listed in Islam's paper, which is good, e.g. Sparc, S100a6, Vim, Fn1.
-				de.out <- topTags(res, n=Inf)
-				write.table(file=paste0("outNB_", datatype, ".tsv"), de.out, row.names=TRUE, 
-					quote=FALSE, sep="\t", col.names=NA)
-				diag.done <- TRUE
-			}
-		}
-
-		final <- do.call(rbind, results)
-        colnames(final) <- c("Lost", "Gained")
-		top.lost <- do.call(rbind, top.res)
-        colnames(top.lost) <- paste0("Top", top.hits)
-   
-        write.table(file=temp, data.frame(Dataset=datatype, Variance=my.var, Method="edgeR (LRT)", Total=length(original), Detected=sum(original),
-            final, top.lost), row.names=FALSE, col.names=!filled, quote=FALSE, sep="\t", append=filled)
-		filled <- TRUE
-	}
-
-	#################################################################################
-	# Finally, monocle.
+	# Next, monocle.
 
 	for (my.var in c(0.001, 0.01, 0.1)) {
 		set.seed(3742)
@@ -185,44 +132,90 @@ for (datatype in c("brennecke", "islam")) {
 			if (i) { spike.data <- resampleSpikes(spike.param, var.log=my.var) }
 			else { spike.data <- spike.param$counts }
 
-			# Normalizing by spike-in total counts.
+			# Normalizing by spike-in total counts (scaled to the total library size).
 			spike.totals <- colSums(spike.data)
 			keep <- !is.spike & filter.keep
-			normalized <- cpm(counts[keep,], lib.size=spike.totals)
+            spike.totals <- spike.totals/mean(spike.totals) * mean(colSums(counts[keep,]))
+			normalized <- edgeR::cpm(counts[keep,], lib.size=spike.totals)
 
 			pdat <- AnnotatedDataFrame(data=data.frame(grouping=grouping))
 			sampleNames(pdat) <- colnames(normalized)
 			HSMM <- newCellDataSet(cellData=normalized, phenoData=pdat)
 
-            if (datatype=="islam") { # for convenience, otherwise we'll be here for days. 
-                HSMM <- HSMM[1:2000,]
-            }
+            HSMM <- HSMM[1:2000,] # for convenience, otherwise we'll be here for days. 
 			out <- differentialGeneTest(HSMM, fullModelFormulaStr="expression~grouping", cores=10) 
 
 			# Choosing stuff. 
-			chosen <- out$qval <= fdr.threshold
-            my.rank <- rank(out$pval) 
-            top.ranked <- lapply(top.hits, function(x) { my.rank <= x })
+			chosen <- p.adjust(out$pval, method="BH") <= fdr.threshold
+		    top.ranked <- rank(out$pval) <= top.hits
 			if (i) { 
-				lost <- sum(original & !chosen)/sum(original)
-				gained <- sum(!original & chosen)/sum(chosen)
-				results[[i]] <- c(lost, gained)
-                top.res[[i]] <- sapply(seq_along(top.hits), function(j) { sum(best[[j]] & !top.ranked[[j]])/top.hits[j] })
-            } else {
+				results[[i]] <- sum(original!=chosen)/sum(original)
+				top.set[[i]] <- sum(best!=top.ranked)/top.hits
+			} else {
 				original <- chosen
-                best <- top.ranked
+				best <- top.ranked
 			}
 		}
 
 		final <- do.call(rbind, results)
-        colnames(final) <- c("Lost", "Gained")
-		top.lost <- do.call(rbind, top.res)
+        colnames(final) <- "Total"
+		top.lost <- do.call(rbind, top.set)
         colnames(top.lost) <- paste0("Top", top.hits)
-   
+
         write.table(file=temp, data.frame(Dataset=datatype, Variance=my.var, Method="monocle", Total=length(original), Detected=sum(original),
             final, top.lost), row.names=FALSE, col.names=!filled, quote=FALSE, sep="\t", append=filled)
 		filled <- TRUE
-	}
+    }
+
+    #################################################################################
+    # Finally, MAST.
+
+    for (my.var in c(0.001, 0.01, 0.1)) {
+        set.seed(3742)
+        results <- top.set <- list()
+    
+        for (i in seq(0,20)) {
+            # Running original data, then resampled versions.
+            if (i) { spike.data <- resampleSpikes(spike.param, var.log=my.var) }
+            else { spike.data <- spike.param$counts }
+
+            # Normalizing by spike-in total counts (again, scaled to the total library size).
+            spike.totals <- colSums(spike.data)
+            keep <- !is.spike & filter.keep
+            spike.totals <- spike.totals/mean(spike.totals) * mean(colSums(counts[keep,]))
+            lcpms <- log2(t(counts[keep,]+1)/spike.totals*1e6)
+
+            suppressMessages({
+                sca <- FromMatrix('SingleCellAssay', lcpms, data.frame(wellKey=rownames(lcpms)), data.frame(primerid=colnames(lcpms)))
+                cData(sca) <- cbind(cData(sca), grouping)
+                cData(sca)$cngeneson <- colMeans(counts>0)
+                                                          
+                mfit <- zlm.SingleCellAssay(~ grouping + cngeneson, sca, method="bayesglm", ebayes=TRUE, ebayesControl=list(method="MLE", model="H1"))
+                mlrt <- lrTest(mfit, Hypothesis(colnames(design)[ncol(design)], colnames(coef(mfit, "D")))) # cngeneson is added at the end, so it shouldn't affect terms before it.
+                MAST.p <- mlrt[, "hurdle", "Pr(>Chisq)"]
+            })                                                                            
+        
+            # Choosing stuff. 
+			chosen <- p.adjust(MAST.p, method="BH") <= fdr.threshold
+		    top.ranked <- rank(MAST.p) <= top.hits
+			if (i) { 
+				results[[i]] <- sum(original!=chosen)/sum(original)
+				top.set[[i]] <- sum(best!=top.ranked)/top.hits
+			} else {
+				original <- chosen
+				best <- top.ranked
+			}
+		}
+
+		final <- do.call(rbind, results)
+        colnames(final) <- "Total"
+		top.lost <- do.call(rbind, top.set)
+        colnames(top.lost) <- paste0("Top", top.hits)
+
+        write.table(file=temp, data.frame(Dataset=datatype, Variance=my.var, Method="MAST", Total=length(original), Detected=sum(original),
+            final, top.lost), row.names=FALSE, col.names=!filled, quote=FALSE, sep="\t", append=filled)
+		filled <- TRUE
+    }
 }
 
 #################################################################################
