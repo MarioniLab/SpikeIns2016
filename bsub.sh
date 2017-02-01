c33e0d5e6d511dc57e295263c6994ec78d133abb
Aaron Lun <alun@wehi.edu.au>
2017-02-01 19:24:39 +0000

    Added bsub script.

---
+++ bsub.sh
@@ -0,0 +1,11 @@
+cd simulations/diffexp
+bsub -R "rusage[mem=10000]" -n 10 -e "log.err" -o "log.out" R CMD BATCH --no-save desim.R
+cd -
+
+cd simulations/variance
+bsub -R "rusage[mem=5000]" -n 1 -e "log.err" -o "log.out" R CMD BATCH --no-save varsim.R
+cd -
+
+cd simulations/clustering
+bsub -R "rusage[mem=5000]" -n 1 -e "log.err" -o "log.out" R CMD BATCH --no-save dimsim.R
+cd - 
