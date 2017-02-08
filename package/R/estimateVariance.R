67297f6815de5584cdb9a211628a368366d5e9c0
Aaron Lun <alun@wehi.edu.au>
2017-02-08 09:22:15 +0000

    Fixed bug in stderr calculation.

--- package/R/estimateVariance.R
+++ package/R/estimateVariance.R
@@ -49,6 +49,7 @@ estimateVariance <- function(y, design, ..., ratios, getfit=FALSE)
 #
 # written by Aaron Lun
 # created 26 January 2016
+# last modified 8 February 2017
 {
     if (missing(ratios)) { ratios <- y$samples$ratio }
     if (missing(design)) { design <- .make_intercept(length(ratios)) }
@@ -57,7 +58,9 @@ estimateVariance <- function(y, design, ..., ratios, getfit=FALSE)
     if (getfit) { return(fit) }
 
     var.est <- sum(fit$effects[-seq_len(fit$rank)]^2)/fit$df.residual
-    attributes(var.est)$standard.error <- var.est/sqrt(nrow(design)-ncol(design))
+    df <- nrow(design) - ncol(design)
+    attributes(var.est)$standard.error <- var.est/sqrt(df) * sqrt(2) # std.err for variance est.
+    attributes(var.est)$df <- df # also returning d.f.
     return(var.est)
 }
 
