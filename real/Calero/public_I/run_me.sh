9990df6db4956d339846e0a3d40d31bfab056652
Aaron Lun <alun@wehi.edu.au>
2017-06-10 18:36:43 +0100

    Fixed reference to wrong SDRF file.

--- real/Calero/public_I/run_me.sh
+++ real/Calero/public_I/run_me.sh
@@ -1,3 +1,3 @@
 ln -s ../../ArrayExpress/counts_Calero_20160113.tsv genic_counts.tsv
-ln -s ../../ArrayExpress/sdrf.tsv sdrf.tsv
+ln -s ../../ArrayExpress/E-MTAB-5522.sdrf.txt sdrf.tsv
 echo "knitr::knit('public_Calero.Rmd')" | R --no-save 
--- real/Calero/public_II/run_me.sh
+++ real/Calero/public_II/run_me.sh
@@ -1,3 +1,3 @@
 ln -s ../../ArrayExpress/counts_Calero_20160325.tsv genic_counts.tsv
-ln -s ../../ArrayExpress/sdrf.tsv sdrf.tsv
+ln -s ../../ArrayExpress/E-MTAB-5522.sdrf.txt sdrf.tsv
 echo "knitr::knit('public_Calero.Rmd')" | R --no-save 
