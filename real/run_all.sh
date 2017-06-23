f1a1dfb4dd4b33336d9f6cc629d11161614c9aa1
Aaron Lun <alun@wehi.edu.au>
2017-06-23 13:23:25 +0100

    Minor fixes to real analysis scripts.

--- real/run_all.sh
+++ real/run_all.sh
@@ -4,6 +4,6 @@ set -u
 for f in Calero/trial_20160113 Calero/trial_20160325 Liora/test_20160906 Liora/test_20170201
 do
     cd $f/analysis
-    echo "knitr::knit('techanal.Rmd')" | R --no-save --vanilla;
+    echo "knitr::knit('techanal.Rmd')" | Rdevel --no-save --vanilla;
     cd -
 done
