afea67839ef8bbc89128fff7d8e2378bbeb2c045
Aaron Lun <alun@wehi.edu.au>
2017-06-23 13:13:15 +0100

    Added docs for spike-in simulation functions. Switched to tagwise dispersion for simulation parameters. Added support for design matrix when estimating parameters.

--- package/R/resampleSpikes.R
+++ package/R/resampleSpikes.R
@@ -1,20 +1,3 @@
-spikeParam <- function(spike.counts) 
-# This computes the parameters of the spike-in counts, including the
-# fitted values and the dispersions used for rescaling later.
-#
-# written by Aaron Lun
-# created 14 October 2015
-# last modified 29 January 2016    
-{
-	spike.counts <- as.matrix(spike.counts)
-	totals <- colSums(spike.counts)
-	dispersion <- estimateDisp(DGEList(spike.counts, lib.size=totals), 
-		design=cbind(rep(1, length(totals))), tagwise=FALSE)$trended.dispersion # Fits are usually good.
-	central.mu <- exp(mglmOneGroup(spike.counts, dispersion=dispersion, offset=log(totals)))
-	cur.mus <- outer(central.mu, totals)
-	return(list(counts=spike.counts, lib.size=totals, mean=central.mu, fitted=cur.mus, dispersion=dispersion))
-}
-
 resampleSpikes <- function(param, var.log)
 # Removes the putative spike-in variability from the fitted values of the
 # original data, and then randomly adds it back in (assuming variance refers to
@@ -24,19 +7,22 @@ resampleSpikes <- function(param, var.log)
 #
 # written by Aaron Lun
 # created 14 October 2015
-# last modified 4 May 2016 
+# last modified 23 June 2017
 {
 	# Removing the effect of spike-in variability from the original library sizes.
-	log.lib <- log2(param$lib.size)
+	log.lib <- log2(param$totals)
 	cur.var <- var(log.lib)
 	if (cur.var < var.log) { warning("variance in original library sizes is lower than specified") }
     new.log.lib <- (log.lib - mean(log.lib)) * sqrt(max(0, 1 - var.log/cur.var)) + mean(log.lib)
 	new.lib.size <- 2^new.log.lib
 
-	# Adding spike-in variability back in, randomly. Note that new.fitted is equivalent to
-    # param$fitted * new.lib.size/param$lib.size, which is how we parameterized it in the paper.
+	# Adding spike-in variability back in, randomly. 
 	fc <- 2^rnorm(length(new.lib.size), 0, sd=sqrt(var.log))
-	new.fitted <- outer(param$mean, fc*new.lib.size)
+    new.lib.size <- new.lib.size * fc
+
+    # Computing new fitted values by scaling. No need to change the dispersion, as we're not
+    # systematically shifting the mean; there should be no need to consider the mean-variance relationship.
+	new.fitted <- t(t(param$fitted) * new.lib.size/param$totals)
 	new.counts <- edgeR::q2qnbinom(param$counts, input=param$fitted, output=new.fitted, dispersion=param$dispersion)
 
     new.counts <- round(pmax(new.counts, 0)) # Making it reasonably count-like.
---
+++ package/R/spikeParam.R
@@ -0,0 +1,18 @@
+spikeParam <- function(spike.counts, design) 
+# This computes the parameters of the spike-in counts, including the
+# fitted values and the dispersions used for rescaling later.
+#
+# written by Aaron Lun
+# created 14 October 2015
+# last modified 23 June 2017
+{
+	spike.counts <- as.matrix(spike.counts)
+    if (missing(design)) { design <- .make_intercept(ncol(spike.counts)) }
+	totals <- colSums(spike.counts)
+    offsets <- log(totals)
+
+	dispersion <- estimateDisp(spike.counts, offset=offsets, design=design, prior.df=0)$tagwise.dispersion
+    fitted <- glmFit(spike.counts, design=design, offset=offsets, dispersion=dispersion, prior.count=0)$fitted.values
+	return(list(counts=spike.counts, totals=totals, fitted=fitted, dispersion=dispersion))
+}
+
