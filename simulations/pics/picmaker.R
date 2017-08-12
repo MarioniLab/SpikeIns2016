431d1a16aba16c4c20f4a8efadc30f43ca8f7dfc
Aaron Lun <alun@wehi.edu.au>
2017-08-12 18:57:07 +0100

    Updated DE plot generator.

--- simulations/pics/picmaker.R
+++ simulations/pics/picmaker.R
@@ -7,41 +7,41 @@ par(mar=c(3.1, 5.1, 2.1, 2.1), mfrow=c(2,1))
 de.results <- read.table("~/AaronDocs/Research/SpikeIns/simulations/diffexp/results.txt", header=TRUE, sep="\t", stringsAsFactor=FALSE)    
 de.results <- de.results[de.results$Variance==0.015,]
 
-calero.de <- de.results[de.results$Dataset=="calero",]
-c.lost <- split(calero.de$AllDE*100, calero.de$Method)
-c.rank <- split(calero.de$Top200*100, calero.de$Method)
-cl.means <- sapply(c.lost, FUN=mean)
-cl.stder <- sapply(c.lost, FUN=function(x) sqrt(var(x)/length(x)))
-cr.means <- sapply(c.rank, FUN=mean)
-cr.stder <- sapply(c.rank, FUN=function(x) sqrt(var(x)/length(x)))
+l.means <- r.means <- l.stder <- r.stder <-  list()
+for (dataset in unique(de.results$Dataset)) { 
+    current <- de.results[de.results$Dataset==dataset,]
+    c.lost <- split(current$AllDE*100, current$Method)
+    c.rank <- split(current$Top200*100, current$Method)
+    l.means[[dataset]] <- sapply(c.lost, FUN=mean)
+    l.stder[[dataset]] <- sapply(c.lost, FUN=function(x) sqrt(var(x)/length(x)))
+    r.means[[dataset]] <- sapply(c.rank, FUN=mean)
+    r.stder[[dataset]] <- sapply(c.rank, FUN=function(x) sqrt(var(x)/length(x)))
+}
 
-islam.de <- de.results[de.results$Dataset=="islam",]
-i.lost <- split(islam.de$AllDE*100, islam.de$Method)
-i.rank <- split(islam.de$Top200*100, islam.de$Method)
-il.means <- sapply(i.lost, FUN=mean)
-il.stder <- sapply(i.lost, FUN=function(x) sqrt(var(x)/length(x)))
-ir.means <- sapply(i.rank, FUN=mean)
-ir.stder <- sapply(i.rank, FUN=function(x) sqrt(var(x)/length(x)))
+l.means <- do.call(rbind, l.means)
+r.means <- do.call(rbind, r.means)
+l.stder <- do.call(rbind, l.stder)
+r.stder <- do.call(rbind, r.stder)
 
-means <- rbind(cl.means, cr.means, il.means, ir.means)
-spacing <- matrix(rep(c(0.2, 0), length(means)/2), ncol=2)
-spacing[1,1] <- 0
-spacing[1,2] <- 2
+combined.means <- rbind(l.means[,1], r.means[,1], l.means[,2], r.means[,2])
+combined.stder <- rbind(l.stder[,1], r.stder[,1], l.stder[,2], r.stder[,2])
+upper <- combined.means + combined.stder
 
-ylim <- 10
-all.cols <- c("blue", "lightblue", "forestgreen", "lightgreen")
-out <- barplot(means, beside=TRUE, col=all.cols, ylab='Change in the DEG set (%)', 
-               ylim=c(0, ylim), cex.lab=1.4, cex.axis=1.2, cex.names=1.4, space=spacing)
+all.cols <- c("forestgreen", "lightgreen", "red", "pink")
+out <- barplot(combined.means, beside=TRUE, col=all.cols, ylab='Change in the DEG set (%)', 
+               ylim=c(0, max(upper)), cex.lab=1.4, cex.axis=1.2, cex.names=1.4)
 
-stderrs <- rbind(cl.stder, cr.stder, il.stder, ir.stder)
-segments(out, means, out, means+stderrs)
-segments(out-0.2, means+stderrs, out+0.2, means+stderrs)
+segments(out, combined.means, out, upper)
+segments(out-0.2, upper, out+0.2, upper)
 
-legend(0, ylim, fill=all.cols, cex=1.4, 
-        legend=c("416B (all DEGs)", "416B (top 200)", "mESC (all DE)", "mESC (top 200)")) 
+legend("topright", fill=all.cols, cex=1.4,
+       legend=c(expression("edgeR, all"), 
+                expression("edgeR, top 200"), 
+                "MAST, all",
+                "MAST, top 200"))
 
 curcoords <- par()$usr
-mtext("a", line=0, cex=1.5, at=curcoords[1] - 0.14*(curcoords[2] - curcoords[1]), font=2)
+mtext("a", line=0, cex=1.5, at=curcoords[1]-1, font=2)
 
 # Making plots of how HVG results change.
 
