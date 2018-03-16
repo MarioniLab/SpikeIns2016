966a92b546c61fe230ab562ed4314eea14db5156
Aaron Lun <alun@wehi.edu.au>
2018-03-16 19:50:20 +0000

    Updated genome build path for a customized local build.

--- real/Calero/trial_20160113/mapme.sh
+++ real/Calero/trial_20160113/mapme.sh
@@ -1,5 +1,5 @@
 ispet=0
 fastq=($(ls fastq/*.fq.gz))
-genome=/lustre/jmlab/resources/genomes/subread/mm10_ERCC_SIRV_onco
+genome=../../../sequences/genomes/mm10_ERCC_SIRV_onco
 
 source ${HOME}/Code/mapping/multi_align.sh
--- real/Liora/test_20170201/mapme.sh
+++ real/Liora/test_20170201/mapme.sh
@@ -1,5 +1,5 @@
 ispet=1
 fastq=($(ls cram/*.cram))
-genome=/lustre/jmlab/resources/genomes/subread/mm10_ERCC_SIRV
+genome=../../../sequences/genomes/mm10_ERCC_SIRV
 
 source ${HOME}/Code/mapping/multi_align.sh
