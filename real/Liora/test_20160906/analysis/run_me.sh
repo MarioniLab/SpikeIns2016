70dc60e5dbe8af4d01387a2326dae10abc26be75
Aaron Lun <alun@wehi.edu.au>
2018-03-17 11:54:06 +0000

    Simplify non-local running of the Rmarkdown scripts.

--- real/Liora/test_20160906/analysis/run_me.sh
+++ real/Liora/test_20160906/analysis/run_me.sh
@@ -1,4 +1,2 @@
-ln -s ../../../ArrayExpress/counts_Liora_20160906.tsv genic_counts.tsv
-ln -s ../../../ArrayExpress/E-MTAB-5522.sdrf.txt sdrf.tsv
 echo ".spike.local <- FALSE; knitr::knit('techanal.Rmd')" | R --no-save 
 
